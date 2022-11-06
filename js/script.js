d2dab9dc0062d357cccc3d53dead2cd25cd6dea4
Sebastian Kowalski <wwy.skowalski@wp.pl>
2022-11-06 20:45:28 +0100

    Add get 'href' attribute from the clicked link, find the correct article using the selector (value of 'href' attribute), add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function titleClickHandler(event){
+    event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked!');
+    console.log('Link was clicked!'); 
 
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -23,10 +24,15 @@ function titleClickHandler(event){
     }
 
     /* get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
 
     /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
 
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active')
 }
 
 const links = document.querySelectorAll('.titles a');
