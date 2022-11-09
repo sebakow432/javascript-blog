ee106646746af70c420563f3f5ccc230cd921ebc
Sebastian Kowalski <wwy.skowalski@wp.pl>
2022-11-09 11:04:35 +0100

    Add generateTitleLinks function (clicking on links does not work *BUG*)

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+/*function printMessage(msg){
+	var div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}*/
+
+function clearMessages(deletedItem){
+	deletedItem.innerHTML = '';
+}
--- js/script.js
+++ js/script.js
@@ -23,15 +23,15 @@ function titleClickHandler(event){
     activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
     console.log(articleSelector);
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
     console.log(targetArticle);
 
-    /* add class 'active' to the correct article */
+    /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active')
 }
 
@@ -40,3 +40,44 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+const optArticleSelector = '.post', optTitleSelector = '.post-title', optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+console.log('Wywołanie funkcji generateTitleLinks')
+
+    /* [DONE] remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    clearMessages(titleList);
+    console.log('remove contents of titleList');
+
+    /* [DONE] for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = '';
+
+    for(let article of articles){
+      console.log(article);
+
+      /* [DONE] get the article id */
+      const articleId = article.getAttribute('id');
+      console.log('Id artykułu - ' + articleId);
+      
+      /* [DONE] find the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      console.log('Zawartość .post-title - ' + articleTitle);
+
+      /* [DONE] get the title from the title element */
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      console.log(linkHTML);
+
+      /* [DONE] create HTML of the link */
+      html = html + linkHTML;
+      console.log('Tak rozbudowuję się kod HTML: ' + html)
+    }
+
+     /* [DONE] insert link into titleList */
+     titleList.insertAdjacentHTML('beforeend',html);
+}
+
+generateTitleLinks();
